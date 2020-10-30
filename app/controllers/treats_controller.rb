b716309de29779c84a437f0cdbe828f2c38b7ab8
brewchetta <chett.tiller@gmail.com>
2020-10-30 18:03:51 -0400

    Session two

--- app/controllers/treats_controller.rb
+++ app/controllers/treats_controller.rb
@@ -1,4 +1,5 @@
 class TreatsController < ApplicationController
+  before_action :find_treat, only: [:show, :edit, :update, :destroy]
 
   def index
     @treats = Treat.all
@@ -13,7 +14,28 @@ class TreatsController < ApplicationController
     redirect_to treats_path
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @treat.update(treat_params)
+    redirect_to treat_path(@treat)
+  end
+
+  def destroy
+    @treat.destroy
+    redirect_to treats_path
+  end
+
   private
+
+  def find_treat
+    @treat = Treat.find_by_id(params[:id])
+    redirect_to treats_path if !@treat
+  end
 
   def treat_params
     params.require(:treat).permit(:name, :picture_url, :ingredients, :rating)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/treats', to: 'treats#index', as: 'treats'
-  post '/treats', to: 'treats#create'
-  get '/treats/new', to: 'treats#new', as: 'new_treat'
+  resources :treats
+  # get '/treats', to: 'treats#index', as: 'treats'
+  # post '/treats', to: 'treats#create'
+  # get '/treats/new', to: 'treats#new', as: 'new_treat'
+  # get '/treats/:id', to: 'treats#show', as: 'treat'
+  # patch '/treats/:id', to: 'treats#update'
 end
